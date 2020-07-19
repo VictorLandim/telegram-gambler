57e2b9b4d4ce9139b911e7f93749760573f706a3
Victor Landim <victorlandim5@gmail.com>
2020-07-19 22:14:38 +0200

    Fix structure

--- dist/src/util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.waitFor = void 0;
-var waitFor = function (timeMs) {
-    return new Promise(function (resolve) { return setTimeout(resolve, timeMs); });
-};
-exports.waitFor = waitFor;
